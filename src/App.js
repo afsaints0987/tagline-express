8c29479a16fb2aa160495cfe91657571d49687dd
afsaints0987 <saints0987@gmail.com>
2022-12-29 16:45:17 +0800

    update changes on ui

--- src/App.js
+++ src/App.js
@@ -43,52 +43,76 @@ function App() {
   }
 
   return (
-    <div className="container text-center">
-      <img src={taglineLogo} alt="tagline-logo" className="my-2" />
-      <div className="d-flex flex-row justify-content-center">
-        <p className="mt-1"><strong>Create your Tagline</strong></p>
-        <TagTooltip />
-      </div>
-      <div className="input-group mb-3 mx-auto">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Write a tagline for..."
-          onChange={handleChange}
-          value={prompt}
-        />
-        <button
-          className="btn btn-outline-danger"
-          type="button"
-          id="button-addon2"
-          onClick={() => generateTagline()}
-        >
-          Give it to me!
-        </button>
-      </div>
-      {error && <p className="text-danger">Dude! I can't see your tagline request</p>}
-      <div>
-        {loading ? (
-          <LoadingSpinner />
-        ) : (
-          tagline && (
-          <div className="bg-danger text-light py-3 px-2 rounded-4 shadow mt-4">
-            <p className="text-center">Here's your tagline...</p>
-            <h3 className="text-center">{tagline}</h3>
-          </div>
-          )
+    <div className="text-center">
+      <header className="bg-dark text-lg-end text-center text-light ">
+        <div className="py-2 px-4 rounded-2 mx-4 badge">
+          <strong>Support:</strong>
+          <a
+            href="https://paypal.me/saints0987"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaIcons.FaPaypal size={30} className="text-primary mx-2" />
+          </a>
+          <a
+            href="https://www.buymeacoffee.com/afsaints0987"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png"
+              alt="Buy Me A Coffee"
+              style={{ height: "30px" }}
+            />
+          </a>
+        </div>
+      </header>
+      <div className="container" id="tagline_container">
+        <img src={taglineLogo} alt="tagline-logo" className="my-2" />
+        <div className="d-flex flex-row justify-content-center">
+          <p className="mt-1">
+            <strong>Create your Tagline</strong>
+          </p>
+          <TagTooltip />
+        </div>
+        <div className="input-group mb-3 mx-auto shadow-sm">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Write a tagline for..."
+            onChange={handleChange}
+            value={prompt}
+          />
+          <button
+            className="btn btn-outline-danger"
+            type="button"
+            id="button-addon2"
+            onClick={() => generateTagline()}
+          >
+            Give it to me!
+          </button>
+        </div>
+        {error && (
+          <p className="text-danger">Dude! I can't see your tagline request</p>
         )}
+        <div>
+          {loading ? (
+            <LoadingSpinner />
+          ) : (
+            tagline && (
+              <div className="bg-danger text-light py-3 px-2 rounded-4 shadow mt-4">
+                <p className="text-center">Here's your tagline...</p>
+                <h3 className="text-center" id="tagline-text">{tagline}</h3>
+              </div>
+            )
+          )}
+        </div>
       </div>
 
       <footer>
         <h6 className="text-center fixed-bottom text-muted">
-          Tagline Express &copy; {new Date().getFullYear()} 
+          Tagline Express &copy; {new Date().getFullYear()}
         </h6>
-        <div className="fixed-top text-lg-end mx-3 mt-3 text-center bg-dark text-light py-2 px-4 rounded-2 mx-4 badge">
-          <strong>Support:</strong>  
-          <a href="https://paypal.me/saints0987" target="_blank" rel="noreferrer"><FaIcons.FaPaypal size={30} className="text-primary mx-2"/></a>
-          <a href="https://www.buymeacoffee.com/afsaints0987" target="_blank" rel="noreferrer"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Buy Me A Coffee" style={{height: "30px"}}/></a>
-        </div>
       </footer>
     </div>
   );
--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -4,11 +4,11 @@ import * as FaIcons from "react-icons/fa"
 
 const popover = (
   <Popover id="popover-basic" >
-    <Popover.Header as="h3" className="text-light bg-danger">Here's how?</Popover.Header>
+    <Popover.Header as="h3" className="text-light bg-danger">Here's how:</Popover.Header>
     <Popover.Body>
       <ul className="text-start">
         <li>Write a tagline for your business, brand and service by starting with "Write a tagline for..." (ex. Write a tagline for shoe business)</li>
-        <li>Press the "Give it to me" to create a tagline</li>
+        <li>Press "Give it to me" to create a tagline</li>
       </ul>
     </Popover.Body>
   </Popover>
@@ -16,7 +16,7 @@ const popover = (
 
 export const TagTooltip = () => {
   return (
-    <OverlayTrigger trigger="click" placement="top" overlay={popover}>
+    <OverlayTrigger trigger="click" placement="top" overlay={popover} rootClose>
       <button type="button" className="btn btn-transparent btn-sm mb-4">
         <FaIcons.FaInfoCircle className="text-dark border-none" />
       </button>
